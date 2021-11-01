e32a75f51731b679a65fc91f828d1c7b2989e8ef
DiegoRodrigoGuevaraDeSantiago <santiago__diego@hotmail.com>
2021-10-31 21:04:54 -0600

    cotizaciones/clientes

--- src/app/infoclientes/infoclientes.page.ts
+++ src/app/infoclientes/infoclientes.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-infoclientes',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoclientesPage implements OnInit {
 
-  constructor() { }
+  constructor(private menu: MenuController, public router: Router) { }
 
   ngOnInit() {
+  }
+
+  openFirst() {
+    this.menu.enable(true, 'first7');
+    this.menu.open('first7');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+  mensajeria(){
+    this.menu.close();
+    this.router.navigateByUrl("/mensajeria");
+  }
+  cotizaciones(){
+    this.menu.close();
+    this.router.navigateByUrl("/cotizaciones");
+  }
+  informacionCliente(){
+    this.menu.close();
+    this.router.navigateByUrl("/infoclientes");
+  }
+  cerrarSesion(){
+    this.menu.close();
   }
 
 }
